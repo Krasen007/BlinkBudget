f0f7a0fc314430e1231b69bf5b029b10c60dc9a6
Krasen Ivanov <krasen007@gmail.com>
2026-02-18 23:21:51 +0200

    update shell commands

--- changelog.bat
+++
@@ -1,2 +0,0 @@
-gemini -p "read @changelog.txt and update it for github changelog page, summarize it in few notes but reword it so it's more easily understood, update the changelog.txt file itself" -y
-pause
